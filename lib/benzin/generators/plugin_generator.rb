1ab8f52a35c72ae4142444bcf715d96c51b14cb1
Curtis Ekstrom <ce@canvus.io>
2014-01-04 04:09:11 +0100

    Add default class options to create dummy app and and mountable engine

--- lib/benzin/generators/plugin_generator.rb
+++ lib/benzin/generators/plugin_generator.rb
@@ -4,14 +4,16 @@ require 'rails/generators/rails/plugin_new/plugin_new_generator'
 
 module Benzin
   class PluginGenerator < Rails::Generators::PluginNewGenerator
-    class_option :database, :type => :string, :aliases => '-d', :default => 'postgresql',
-      :desc => "Preconfigure for selected database (options: #{DATABASES.join('/')})"
+    class_option :database, type: :string, aliases: '-d', default: 'postgresql',
+      desc: "Preconfigure for selected database (options: #{DATABASES.join('/')})"
 
-    class_option :skip_test_unit, :type => :boolean, :aliases => '-T', :default => true,
-      :desc => 'Skip Test::Unit files'
+    class_option :skip_test_unit, type: :boolean, aliases: '-T', default: true, desc: 'Skip Test::Unit files'
 
-    class_option :skip_bundle, :type => :boolean, :aliases => '-B', :default => true,
-      :desc => "Don't run bundle install"
+    class_option :skip_bundle, type: :boolean, aliases: '-B', default: true, desc: "Don't run bundle install"
+
+    class_option :mountable, type: :boolean, default: true, desc: "Generate mountable isolated application"
+
+    class_option :dummy_path, type: :string, default: "spec/dummy", desc: "Create dummy application at given path"
 
     def finish_template
       invoke :benzin_customization
